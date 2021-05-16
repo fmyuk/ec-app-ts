8d8c0b40a6565c8493cf509639a1e4bc600249f7
fumi uk <dcfumiya.238@gmail.com>
2021-05-17 08:47:25 +0900

    ec-app-ts

---
+++ src/firebase/config.ts
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+  apiKey: "AIzaSyBp2d7YlUsDU3uMafmPsZ2qpTdHy9Ahik0",
+  authDomain: "ec-app-ts.firebaseapp.com",
+  projectId: "ec-app-ts",
+  storageBucket: "ec-app-ts.appspot.com",
+  messagingSenderId: "6024796209",
+  appId: "1:6024796209:web:17247020fea7f7a8e2dd10",
+  measurementId: "G-HMLVF1QB5K"
+};
